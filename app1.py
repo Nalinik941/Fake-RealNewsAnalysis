ca7b5d93901bdc6e4e7caf029c6e2460dc391c7e
nalini <nalinik941@gmail.com>
2025-01-25 15:44:47 +0530

    first commit

---
+++ app1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 12:17:36 2025
+
+@author: Dell
+"""
+
+import streamlit as st
+import joblib
+
+vectorizer = joblib.load("C:/Users/Dell/ml_stramlit_app/vectorization.jb")
+model = joblib.load('C:/Users/Dell/ml_stramlit_app/DTC.jb')
+
+st.title("Real or Fake news Analysis")
+st.write("Enter a news Article below")
+
+news_input = st.text_area("News Article:","")
+
+if st.button("Check News"):
+    if news_input.strip():
+        transform_input = vectorizer.transform([news_input])
+        prediction = model.predict(transform_input)
+        
+        
+        if prediction[0]==1:
+            st.success("The News is Real ")
+        else:
+            st.error("The News is Fake ")
+    else:
+        st.warning("Please enter some text to analize. ")
