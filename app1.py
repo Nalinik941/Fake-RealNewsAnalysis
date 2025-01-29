d851c17b474c939c84b57d6f246dda694e30c0d6
nalini <nalinik941@gmail.com>
2025-01-29 13:43:56 +0530

    app

--- app1.py
+++ app1.py
@@ -12,6 +12,12 @@ vectorizer = joblib.load("vectorization.jb")
 model = joblib.load('./GBC.jb')
 
 st.title("Real or Fake news Analysis")
+html_temp = """
+<div style="background-color:tomato;padding:10px">
+<h2 style="color:white;text-align:center;">Streamlit Real and Fake News Analysis app </h2>
+</div>
+"""
+st.markdown(html_temp,unsafe_allow_html=True)
 st.write("Enter a news Article below")
 
 news_input = st.text_area("News Article:","")
@@ -28,3 +34,6 @@ if st.button("Check News"):
             st.error("The News is Fake ")
     else:
         st.warning("Please enter some text to analize. ")
+        
+if st.button("About"):
+    st.text("This app will help us to get Real or Fake news")
