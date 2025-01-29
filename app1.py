50d2a864be7041b034aefe5a90cc750242ea26a9
nalini <nalinik941@gmail.com>
2025-01-29 14:28:19 +0530

    app1

--- app1.py
+++ app1.py
@@ -5,12 +5,15 @@ Created on Sat Jan 25 12:17:36 2025
 @author: Dell
 """
 
+#importing reuired libraries
 import streamlit as st
 import joblib
 
+#loading our vectorization and GBC model saved files) 
 vectorizer = joblib.load("vectorization.jb")
 model = joblib.load('./GBC.jb')
 
+#Title for the website
 st.title("Real or Fake news Analysis")
 html_temp = """
 <div style="background-color:tomato;padding:10px">
@@ -18,14 +21,18 @@ html_temp = """
 </div>
 """
 st.markdown(html_temp,unsafe_allow_html=True)
+
+#Subtitle
 st.write("Enter a news Article below")
 
+#User inputs 
 news_input = st.text_area("News Article:","")
 
+#create a button to check news is real or fake
 if st.button("Check News"):
     if news_input.strip():
-        transform_input = vectorizer.transform([news_input])
-        prediction = model.predict(transform_input)
+        transform_input = vectorizer.transform([news_input]) #transforming the input given by user
+        prediction = model.predict(transform_input) #Transormed input to the model for the prediction
         
         
         if prediction[0]==1:
